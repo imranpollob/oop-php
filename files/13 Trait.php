c018fb1a18b17a792a1f91b2da458c81a904896b
pollmix <polboy777@gmail.com>
2018-12-26 00:38:05 +0600

    update

---
+++ files/13 Trait.php
@@ -0,0 +1,33 @@
+<?php
+// Traits are a mechanism for code reuse in single inheritance languages such as PHP
+// Reuse sets of methods from trait freely in several independent classes
+trait Hello
+{
+    public function sayHello()
+    {
+        echo 'Hello ';
+    }
+}
+
+trait World
+{
+    public function sayWorld()
+    {
+        echo 'World';
+    }
+}
+
+class MyHelloWorld
+{
+    // Multiple Traits Usage 
+    use Hello, World;
+    public function sayExclamationMark()
+    {
+        echo '!';
+    }
+}
+
+$o = new MyHelloWorld();
+$o->sayHello();
+$o->sayWorld();
+$o->sayExclamationMark();
